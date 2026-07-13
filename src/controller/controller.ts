6b9ccaf92192bc5db5f8753a729b6146286ad869
Andrei-Marcel Badulescu <andrei-marcel.badulescu@softwire.com>
2026-07-13 18:04:21 +0300

    [misc] Add Regex expression for Date checking
    Signed-off-by: Andrei-Marcel Badulescu <andrei-marcel.badulescu@softwire.com>

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,13 +1,13 @@
 import express, {type Request, type Response, Router} from "express";
-import fs from "node:fs";
-import { apodObj } from "../model/apodObj";
 import { journalEntry } from "../model/journalEntry";
 import { getRandom, getMultiple, getSpecific } from "../services/dataHandler";
 
 class APODController {
     private app;
     private router;
+
     public readonly port: number = 8000;
+    private readonly dateRegex: RegExp = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
 
     public constructor() {
         this.app = express();
@@ -27,14 +27,19 @@ class APODController {
         this.router.get('/apod/today', async (req, res) => {
 
             let now = new Date();
-            let data: journalEntry[] = await getSpecific(`${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`);
+            let data: journalEntry[] = await getSpecific(now.toISOString().slice(0, 10));
             res.send(data);
 
         });
 
-        this.router.get('/apod/date/:date', async (req: Request<{ userDate: string }>, res: Response) => {
+        this.router.get('/apod/date/:date', async (req: Request<{ date: string }>, res: Response) => {
 
-            let data: journalEntry[] = await getSpecific(req.params.userDate);
+            if (!this.dateRegex.test(req.params.date)) {
+                res.status(400).send("Invalid request!");
+                return;
+            }
+
+            let data: journalEntry[] = await getSpecific(req.params.date);
             res.send(data);
 
         });
@@ -42,7 +47,7 @@ class APODController {
         this.router.get('/apod/random/:count', async (req: Request<{ count: number }>, res: Response) => {
 
             if (req.params.count < 1) {
-                res.send("Invalid request!");
+                res.status(400).send("Invalid request!");
                 return;
             }
 
@@ -51,7 +56,7 @@ class APODController {
 
         });
 
-        this.router.get('/apod/interval/', async (req: Request, res: Response) => {
+        this.router.get('/apod/interval', async (req: Request, res: Response) => {
 
             const start = req.query.start;
             const end = req.query.end;
@@ -59,7 +64,12 @@ class APODController {
             const pagEnd = req.query.pagEnd;
 
             if (typeof start !== "string" || typeof end !== "string") {
-                res.send("Invalid request!");
+                res.status(400).send("Invalid request!");
+                return;
+            }
+
+            if (!(this.dateRegex.test(start) && this.dateRegex.test(end))) {
+                res.status(400).send("Invalid request!");
                 return;
             }
 
@@ -70,7 +80,7 @@ class APODController {
                 let lhs = parseInt(pagStart) - 1;
                 let rhs = parseInt(pagEnd) - 1;
 
-                if (lhs >= 0 && lhs < data.length() && rhs > lhs && rhs <= data.length())
+                if (lhs >= 0 && lhs < data.length && rhs > lhs && rhs <= data.length)
                     data = data.slice(lhs, rhs + 1);
             }
 
